a1d010de9e61a35e5dc77f4f8d9a199f60d18d34
Donghoon Han <apensia914@Donghoons-MacBook-Pro.local>
2021-03-05 23:55:58 +0900

    HTML structuring & CSS styling

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Dropdown Menu
+
+function openMenu() {
+    document.getElementById('myMenu').style.height = '100%';
+}
+
+function closeMenu() {
+    document.getElementById('myMenu').style.height = '0%';
+}
