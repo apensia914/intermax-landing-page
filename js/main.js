0ab160d6ad01e2aa53c686ca1d04207b1c59ee79
Donghoon Han <apensia914@Donghoons-MacBook-Pro.local>
2021-03-06 11:32:28 +0900

    Scroll animation js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@
 
     let yOffset = 0;
     let prevScrollHeight = 0; // Sum of scroll section heights before yOffset
-    let currentScene = 0; // Current scroll-section 
+    let currentScene = 0; // Current section 
+    let enterNewScene = false; // Value will be True once new scene starts 
 
     // Info of each scene
     const sceneInfo = [
@@ -14,7 +15,14 @@
             heightNum: 5, // Browser length adjustment
             scrollHeight: 0, 
             objs: {
-                container: document.querySelector('#scroll-section-0')
+                container: document.querySelector('#scroll-section-0'),
+                messageFirst: document.querySelector('#scroll-section-0 .main-message.first'),
+                messageSecond: document.querySelector('#scroll-section-0 .main-message.second'),
+                messageThird: document.querySelector('#scroll-section-0 .main-message.third'),
+                messageFourth: document.querySelector('#scroll-section-0 .main-message.fourth')
+            },
+            values: {
+                messageFirst_opacity: [0, 1],
             }
         },
         {   
@@ -66,23 +74,63 @@
         document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
+    // 
+    const calcValues = (values, currentYOffset) => { 
+        let rv;
+        // Ratio of scrolled range on the current scene
+        let scrollRatio = currentYOffset / sceneInfo[currentScene].scrollHeight;
+        rv = scrollRatio * (values[1] - values[0]) + values[0];
+        return rv;
+    }
+
+    // Animation 
+    const playAnimation = () => {
+        const objs = sceneInfo[currentScene].objs;
+        const values = sceneInfo[currentScene].values;
+        const currentYOffset = yOffset - prevScrollHeight;
+
+        console.log(currentScene);
+
+        switch (currentScene) {
+            case 0: 
+                let messageFirst_opacity_in = calcValues(values.messageFirst_opacity, currentYOffset);
+                objs.messageFirst.style.opacity = messageFirst_opacity_in;
+                console.log(messageFirst_opacity_in);
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    }
+
+
     // Returning yOffset coordinates on each scroll
     const scrollLoop = () => {
+        enterNewScene = false;
         prevScrollHeight = 0;
         for (let i = 0; i < currentScene; i++) {
             prevScrollHeight += sceneInfo[i].scrollHeight;
         }
 
         if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+            enterNewScene = true;
             currentScene++;
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
         if (yOffset < prevScrollHeight) {
-            if (currentScene === 0) return; 
+            enterNewScene = true;
+            if (currentScene === 0) return; // Prevent bouncing error in Chrome
             currentScene--;
+            document.body.setAttribute('id', `show-scene-${currentScene}`);
         }
 
-        document.body.setAttribute('id', `show-scene-${currentScene}`);
+        if (enterNewScene) return;
+
+        playAnimation();
     }
 
     window.addEventListener('scroll', () => {
